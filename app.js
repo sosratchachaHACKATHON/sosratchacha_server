59259f342d08e0740e425d675a5b0e956c514c8b
Leegunyoung <hdn1015@likelion.org>
2023-05-05 16:39:18 +0900

    환경설정

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+
+app = express()
+server = http.createServer(app);
+
+app.get('/', function (req, res) {
+  res.send('Hello /');
+});
+
+app.get('/world.html', function (req, res) {
+  res.send('Hello World');
+});
+
+server.listen(8000, function () {
+  console.log('Express server listening on port ' + server.address().port);
+});
