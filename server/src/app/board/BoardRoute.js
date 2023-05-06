4762aafd972435818f8b07b28633e5cbbd278164
Jeonjunhowi <jjh63360@kau.kr>
2023-05-06 22:50:16 +0900

    fix: remove token when view boadrd

--- server/src/app/board/BoardRoute.js
+++ server/src/app/board/BoardRoute.js
@@ -6,7 +6,7 @@ module.exports = function(app){
     //1. 사진 및 위치 정보 업로드하기
     app.post('/app/board', imageUploader.any('image'), boardController.createBoard);
     //2. 게시판 조회하기
-    app.get('/app/board', jwt.jwtMiddleware, boardController.getBoard);
+    app.get('/app/board', boardController.getBoard);
     //3. 게시판 댓글 작성하기
     app.post('/app/board/:boardId/comment', jwt.jwtMiddleware, boardController.createComment);
     //4. 게시판 댓글, 내용 조회하기
