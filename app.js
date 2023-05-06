c0b68a85c206e21d734af3a0a8ecec382eb05779
Jeonjunhowi <jjh63360@kau.kr>
2023-05-06 20:06:32 +0900

    add: react front page

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ var expressForStatic = require("express");
 
 //app이라는 변수에 express 함수의 변환 값을 저장한다.
 var app = express();
+
+// 정적 파일 불러오기
+app.use(expressForStatic.static(path.join(__dirname + "/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+})
+
 const port = app.listen(8000);
 // express 서버를 실행할 때 필요한 포트 정의 및 실행 시 callback 함수를 받습니다
 app.listen(port, function() {
     console.log(`start!express server on port 8000`);
 })
+
+
+
 
 module.exports = app;
